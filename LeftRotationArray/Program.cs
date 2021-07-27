a0c549b5131fb41116130163217fda63b1e4f1d8
Uzair1995 <uzairaleem1995@gmail.com>
2021-07-27 15:17:50 +0500

    Adding functionality to ask how many times to rotate in LeftRotationArray

--- LeftRotationArray/Program.cs
+++ LeftRotationArray/Program.cs
@@ -6,21 +6,30 @@ namespace LeftRotationArray
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter space seperated integer array for rotation. Example -> [1 2 3 4 5]: ");
             string[] nd = Console.ReadLine().Split(' ');
 
             int n = Convert.ToInt32(nd[0]);
-
             int d = Convert.ToInt32(nd[1]);
-
             int[] a = Array.ConvertAll(nd, aTemp => Convert.ToInt32(aTemp));
 
-            //function call
-            RotateLeft(n, d, a);
+            Console.WriteLine("Please enter how many times to rotate the array: ");
+            int times = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < times; i++)
+            {
+                Console.Write($"Iteration # {i + 1}: ");
+                a = RotateLeft(n, d, a);
+                
+                for (int j = 0; j < a.Length; j++)
+                    Console.Write(a[j] + " ");
+                Console.WriteLine();
+            }
         }
 
 
-        //length - rot + index
-        static void RotateLeft(int n, int d, int[] a)
+        static int[] RotateLeft(int n, int d, int[] a)
         {
             d = d % n;
             int[] finalArr = new int[a.Length];
@@ -31,11 +40,8 @@ namespace LeftRotationArray
                     index = index % a.Length;
                 finalArr[index] = a[i];
             }
-            //print in , seperated way
-            for (int i = 0; i < finalArr.Length; i++)
-            {
-                Console.Write(finalArr[i] + " ");
-            }
+
+            return finalArr;
         }
 
     }
